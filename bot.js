ce30f4173ae7acce35329b8b02503e38ccfb43be
alexzhmud <31706321+alexzhmud@users.noreply.github.com>
2018-06-14 14:58:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,8 +36,9 @@ bot.dialog('exit', require('./dialogs/exit-dialog.js')).triggerAction({ matches:
 // QNA Dialogs definitions
 var ua_recognizer = new cognitiveservices.QnAMakerRecognizer({
 	knowledgeBaseId: process.env.QNA_UA_KBID, 
-	subscriptionKey: process.env.QNA_UA_KEY,
-    	endpointHostName: process.env.QNA_HOST_NAME});
+	subscriptionKey: process.env.QNA_UA_KEY
+    	//endpointHostName: process.env.QNA_HOST_NAME
+});
 
 var ua_qnadialog = new cognitiveservices.QnAMakerDialog({ 
 	recognizers: [ua_recognizer],
@@ -48,8 +49,9 @@ bot.dialog('ua_qna', ua_qnadialog);
 
 var ru_recognizer = new cognitiveservices.QnAMakerRecognizer({
 	knowledgeBaseId: process.env.QNA_RU_KBID, 
-	subscriptionKey: process.env.QNA_RU_KEY,
-    	endpointHostName: process.env.QNA_HOST_NAME});
+	subscriptionKey: process.env.QNA_RU_KEY
+    	//endpointHostName: process.env.QNA_HOST_NAME
+});
 
 var ru_qnadialog = new cognitiveservices.QnAMakerDialog({ 
 	recognizers: [ru_recognizer],
@@ -60,8 +62,9 @@ bot.dialog('ru_qna', ru_qnadialog);
 
 var en_recognizer = new cognitiveservices.QnAMakerRecognizer({
 	knowledgeBaseId: process.env.QNA_EN_KBID, 
-	subscriptionKey: process.env.QNA_EN_KEY,
-    	endpointHostName: process.env.QNA_HOST_NAME});
+	subscriptionKey: process.env.QNA_EN_KEY
+    	//endpointHostName: process.env.QNA_HOST_NAME
+});
 
 var en_qnadialog = new cognitiveservices.QnAMakerDialog({ 
 	recognizers: [en_recognizer],
